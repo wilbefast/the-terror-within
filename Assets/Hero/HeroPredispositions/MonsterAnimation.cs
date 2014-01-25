1c6d06390fa254dd86f599a7b8727b7755a92b69
tomWHG <tom@whalehammergames.com>
2014-01-25 17:27:50 +1100

    Monster can now move horizontally

--- Assets/Hero/HeroPredispositions/MonsterAnimation.cs
+++ Assets/Hero/HeroPredispositions/MonsterAnimation.cs
@@ -6,6 +6,7 @@ public class MonsterAnimation : MonoBehaviour
 	public bool monsterIsRunning;
 	public float baseLine, verticalSpeed;
 	private float maxVerticalSpeed = -0.05f;
+	private float horizontalSpeed = -0.025f;
 	
 	void Start()
 	{
@@ -28,6 +29,18 @@ public class MonsterAnimation : MonoBehaviour
 				verticalSpeed += maxVerticalSpeed / 40f;
 			}
 			this.transform.Translate (new Vector3(0,verticalSpeed,0));
+		}
+		
+		if(this.transform.position.x > 5.0f)
+		{
+			monsterIsRunning = true;
+			((RunningAnimation)GameObject.FindObjectOfType(typeof(RunningAnimation))).partyIsRunning = true;
+			this.transform.Translate ( new Vector3(horizontalSpeed,0,0));
+		}
+		else
+		{
+			monsterIsRunning = false;
+			((RunningAnimation)GameObject.FindObjectOfType(typeof(RunningAnimation))).partyIsRunning = false;
 		}
 	}
 }
